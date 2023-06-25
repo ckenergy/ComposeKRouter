961b69105c36aaeab3ae12c1db20c58e5c50dd5e
ckenergy <2ckenergy@gmail.com>
2023-06-25 11:57:07 +0800

    完善Readme文档

--- main/src/main/java/com/ckenergy/compose/main/SecondPage.kt
+++ main/src/main/java/com/ckenergy/compose/main/SecondPage.kt
@@ -14,6 +14,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
