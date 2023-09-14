40bcb30502ed07e955774eceb8a4aba90804b32b
ckenergy <2ckenergy@gmail.com>
2023-09-14 14:37:03 +0800

    readme add plugin info

--- main/src/main/java/com/ckenergy/compose/main/SecondPage.kt
+++ main/src/main/java/com/ckenergy/compose/main/SecondPage.kt
@@ -1,8 +1,6 @@
 package com.ckenergy.compose.main
 
-import android.content.Context
 import android.util.Log
-import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -12,19 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
 import com.ckenergy.compose.AppNavController
 import com.ckenergy.compose.MyContent
 import com.ckenergy.compose.common.ComposeRouterMapper
 import com.ckenergy.compose.krouter.core.KRouter
-import com.ckenergy.compose.krouter.core.ModuleBuilder
-import com.ckenergy.compose.krouter.core.NavGraphManager
-import com.ckenergy.compose.krouter.core.PluginManager
 import com.ckenergy.compose.krouter.core.navigateRoute
-import java.io.File
 
 /**
  * @author ckenergy
